135f1b1b4e3fb0b16a69046361d9f3da039bfd7e
ArkkiError <71436744+ArkkiError@users.noreply.github.com>
2020-10-07 14:19:32 +0300

    Update myscript2.sh

--- myscript2.sh
+++ myscript2.sh
@@ -1,9 +1,9 @@
-this command in 'myscript2.sh' starts my 'text_based_game' and opens Visual Studio program.
----------------------------------
+# this command in 'myscript2.sh' starts my 'text_based_game' and opens Visual Studio program.
+# ---------------------------------
 #! /bin/bash
 cd ~/repos/chatbot/School_projects/School_practices/AI_Codelab
 code exam_practice2_game.py
 ./exam_practice2_game.py
----------------------------------
+# ---------------------------------
 
-just write: ./myscript2.sh
+# just write: ./myscript2.sh
